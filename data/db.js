a26e86e81eb66aee8e65f47c42fc511a888c6671
chryscode <chrysanthacode@gmail.com>
2024-08-11 15:04:37 -0400

    Integrate to external openlibrary.org API

--- data/db.js
+++ data/db.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./books-catalogue.db', (err) => {
@@ -7,43 +8,135 @@ const db = new sqlite3.Database('./books-catalogue.db', (err) => {
     console.log('Connected to the database.');
 });
 
-// Create books table if it doesn't exist
-db.run(`
-    CREATE TABLE IF NOT EXISTS books (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT,
-        author TEXT,
-        published_year INTEGER
-    )
-`, (err) => {
+//Create the books table and populate this from an external api
+db.get('SELECT name FROM sqlite_master WHERE type = "table" AND name = "books"', (err, row) => {
     if (err) {
-        console.error(err.message);
-    }
-    console.log('Books table created or already exists.');
-});
-
-db.run(`
-    CREATE TABLE IF NOT EXISTS authors (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      biography TEXT,
-      date_of_birth TEXT
-    )
-  `, (err) => {
-    if (err) {
-      console.error(err);
+        console.error(err);
+    } else if (row) {
+        console.log('Books table already exists.');
     } else {
-      console.log('Authors table created successfully.');
+        console.log('Creating books table...');
+        db.run(`
+            CREATE TABLE books (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                author TEXT NOT NULL,
+                first_publish_year INTEGER
+            )` , (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Books table created successfully.');
+                
+                //Add Books
+                addBooks('Dracula');
+                addBooks('Pride And Prejudice');
+                addBooks('Sense And Sensibility');
+                addBooks('Harry Potter');
+
+
+            }
+        });
     }
 });
 
-function getAllBooks(){
-    db.all('SELECT * FROM books', (err, rows) => {
-        if (err) {
-            return 'Error';
-        }
-        return rows;
-    });
+//Create the authors table and populate this from an external api
+db.get('SELECT name FROM sqlite_master WHERE type = "table" AND name = "authors"', (err, row) => {
+    if (err) {
+        console.error(err);
+    } else if (row) {
+        console.log('Authors table already exists.');
+    } else {
+        console.log('Creating authors table...');
+        db.run(`
+            CREATE TABLE authors (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                birth_date TEXT,
+                top_work TEXT
+            )` , (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Authors table created successfully.');
+                
+                //Add Authors
+                addAuthor('J. K. Rowling');
+                addAuthor('George Orwell');
+                addAuthor('Stephen King');
+                addAuthor('Edgar Allan Poe');
+                addAuthor('C.S.Lewis');
+                addAuthor('Jane Austen');
+                addAuthor('Bram Stocker');
+            }
+        });
+    }
+});
+
+//Calling external Library & adding to the authors
+function addAuthor(authorName){
+    const url = `https://openlibrary.org/search/authors.json?q=${authorName}`;
+
+    https.get(url, (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data +=chunk;
+        });
+
+        res.on('end', () => {
+            try{
+                const response = JSON.parse(data);
+
+                const { name, birth_date, top_work } = response.docs[0];
+                db.run('INSERT INTO authors (name, birth_date, top_work) VALUES (?, ?, ?)', [name, birth_date, top_work], (err) => {
+                    if (err) {
+                      console.error(err);
+                    } else {
+                      console.log('Author inserted successfully.');
+                    }
+                  });
+
+            } catch(error){
+                console.log( { error: error } );
+            }
+        })
+    }).on('error', (error) => {
+        console.log({error: error});
+    })
+}
+
+//Calling external Library & adding to the books
+function addBooks(bookName){
+    const url = `https://openlibrary.org/search.json?q=${bookName}`;
+
+    https.get(url, (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data +=chunk;
+        });
+
+        res.on('end', () => {
+            try{
+                const response = JSON.parse(data);
+
+                const { title, author_name, first_publish_year } = response.docs[0];
+                db.run('INSERT INTO books (title, author, first_publish_year) VALUES (?, ?, ?)', [title, author_name[0], first_publish_year], (err) => {
+                    if (err) {
+                      console.error(err);
+                    } else {
+                      console.log('Book inserted successfully.');
+                    }
+                  });
+
+            } catch(error){
+                console.log( { error: error } );
+            }
+        })
+    }).on('error', (error) => {
+        console.log({error: error});
+    })
 }
 
 module.exports = db;
